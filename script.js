6d5ac7fc4d2cd6415eb5d2bdbe65aaeb78bd12b8
LaptopKing <csordaspeter2002@gmail.com>
2022-02-22 13:29:37 +0100

    Nem teljes de talán megteszi

--- script.js
+++ script.js
@@ -2,6 +2,57 @@ function get_ev() {
 	document.getElementById("ev_v").innerHTML = document.getElementById("ev").value;
 }
 
-function ellenorzes() {
+function ellenoriz() {
+	var nev1 = document.getElementById("nev1").value;
+	var nev2 = document.getElementById("nev2").value;
+	var r1 = document.getElementById("r1").checked;
+	var r2 = document.getElementById("r2").checked;
+	var r3 = document.getElementById("r3").checked;
+	var r4 = document.getElementById("r4").checked;
+	var dropdown = document.getElementById("dropdown").value;
+	var nev3 = document.getElementById("nev3").value;
+	var ev = document.getElementById("ev").value;
 
+	if (nev1 == "Romolus" && nev2 == "Remus")
+	{
+		document.getElementById("1").style.backgroundColor = "green";
+	}
+	else {
+		document.getElementById("1").style.backgroundColor = "red";
+	}
+	switch(true) {
+		case r1:
+			document.getElementById("2").style.backgroundColor = "red";
+			break;
+		case r2:
+			document.getElementById("2").style.backgroundColor = "red";
+			break;
+		case r3:
+			document.getElementById("2").style.backgroundColor = "green";
+			break;
+		case r4:
+			document.getElementById("2").style.backgroundColor = "red";
+			break;
+	}
+	if (dropdown == "patriciusok")
+	{
+		document.getElementById("3").style.backgroundColor = "green";
+	}
+	else {
+		document.getElementById("3").style.backgroundColor = "red";
+	}
+
+	if (nev3 == "Servius Tullius"){
+		document.getElementById("4").style.backgroundColor = "green";
+	}
+	else {
+		document.getElementById("4").style.backgroundColor = "red";
+	}
+
+	if (ev == 476) {
+		document.getElementById("5").style.backgroundColor = "green";
+	}
+	else {
+		document.getElementById("5").style.backgroundColor = "red";
+	}
 }
